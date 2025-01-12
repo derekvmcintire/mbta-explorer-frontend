77915fccd5b1bd6fc417c59755086c5fdd337fa7
derekvmcintire <derekcommonground@gmail.com>
2025-01-11 20:00:34 -0500

    vehicle marker manager file

--- src/explorer-api/batch_live_updates.ts
+++ src/explorer-api/batch_live_updates.ts
@@ -1,110 +1,17 @@
 import { get } from "svelte/store";
-import type {
-  Vehicle,
-  VehicleAttributes,
-  VehicleStateMap,
-} from "../stores/live_track_store";
-import * as L from "leaflet";
 import { vehicleStateMap } from "../stores/live_track_store";
 import { vehicleUpdateQueue } from "../stores/vehicle_update_queue_store";
 import { mapStore } from "../stores/map_store";
-
-const VEHICLE_STATUS_TEXT_MAP = new Map([
-  ["STOPPED_AT", "Stopped"],
-  ["IN_TRANSIT_TO", "In Transit"],
-  ["INCOMING_AT", "Arriving"],
-]);
-
-class VehicleMarkerManager {
-  private static createPopupContent(
-    attributes: VehicleAttributes & { route?: string },
-  ): string {
-    const { current_status, speed, route, updated_at } = attributes;
-    const vehicleStatus =
-      VEHICLE_STATUS_TEXT_MAP.get(current_status || "") || "";
-
-    return `
-      <strong>Route: ${attributes.route || "Not Provided"}</strong><br>
-      Speed: ${speed || "Unknown"}${speed ? "mph" : ""}<br>
-      Status: ${vehicleStatus}<br>
-      Updated At: ${updated_at}<br>
-    `;
-  }
-
-  private static createMarker(
-    map: L.Map | L.LayerGroup<any>,
-    attributes: VehicleAttributes & { route?: string },
-  ): L.Circle {
-    const marker = L.circle([attributes.latitude, attributes.longitude], {
-      color: "yellow",
-      fillColor: "yellow",
-      fillOpacity: 1,
-      radius: 50,
-    });
-
-    marker.bindPopup(this.createPopupContent(attributes));
-    marker.addTo(map);
-    return marker;
-  }
-
-  private static updateMarker(
-    markerState: VehicleStateMap,
-    newAttributes: VehicleAttributes & { route?: string },
-  ): void {
-    const updatedAttributes = { ...markerState.data };
-
-    // Update position
-    updatedAttributes.latitude = newAttributes.latitude;
-    updatedAttributes.longitude = newAttributes.longitude;
-    markerState.marker.setLatLng([
-      newAttributes.latitude,
-      newAttributes.longitude,
-    ]);
-
-    // Update other attributes if they exist and are valid
-    if (newAttributes.direction !== undefined)
-      updatedAttributes.direction = newAttributes.direction;
-    if (newAttributes.speed !== undefined && newAttributes.speed !== 0)
-      updatedAttributes.speed = newAttributes.speed;
-    if (newAttributes.updated_at)
-      updatedAttributes.updated_at = newAttributes.updated_at;
-    if (newAttributes.current_status)
-      updatedAttributes.current_status = newAttributes.current_status;
-    if (newAttributes.bearing !== undefined)
-      updatedAttributes.bearing = newAttributes.bearing;
-    if (newAttributes.route) updatedAttributes.route = newAttributes.route;
-
-    // Update the popup content with new information
-    markerState.marker.setPopupContent(
-      this.createPopupContent(updatedAttributes),
-    );
-
-    // Update the stored data
-    markerState.data = updatedAttributes;
-  }
-
-  static handleVehicleUpdate(
-    vehicle: Vehicle,
-    vehicleStates: Record<string, VehicleStateMap>,
-    map: L.Map,
-  ): Record<string, VehicleStateMap> {
-    const { id, attributes } = vehicle;
-    const route = vehicle?.relationships?.route?.data?.id || "";
-    const enrichedAttributes = { ...attributes, route };
-
-    if (vehicleStates[id]) {
-      this.updateMarker(vehicleStates[id], enrichedAttributes);
-    } else {
-      const marker = this.createMarker(map, enrichedAttributes);
-      vehicleStates[id] = { marker, data: enrichedAttributes };
-    }
-
-    return vehicleStates;
-  }
-}
+import { VehicleMarkerManager } from "./vehicle_marker_manager";
 
 let isProcessing = false;
 
+/**
+ * Processes a batch of vehicle updates from the update queue and updates the map.
+ *
+ * This function retrieves updates from the `vehicleUpdateQueue` store, updates the
+ * state of vehicle markers on the map, and prevents concurrent processing by using a flag.
+ */
 export function processBatchUpdates(): void {
   const map = get(mapStore);
   if (!map || isProcessing) return;
